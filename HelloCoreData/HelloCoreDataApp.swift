703fe122391a4452cc52b50f902849793a836fac
Mojo <mojo@nuralab.com>
2022-02-19 00:48:18 -0600

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by Joseph DeWeese on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
